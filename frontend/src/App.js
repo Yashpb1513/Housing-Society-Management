82d382cdb1e262c6b88ebae6cfc91cb251ff3d82
nupur <nupurvupadhyay1292@gmail.com>
2022-05-16 15:51:37 +0530

    Cards Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ReachUs from "./components/ReachUs";
 import Announcement from "./components/Announcement";
+
 function App() {
   return (
     <div>
@@ -14,6 +15,26 @@ function App() {
         <Route path="/Announcement" element={<Announcement />} />
         <Route path="/ReachUs" element={<ReachUs />} />
       </Routes>
+      <div className="wrapper">
+         <Announcement 
+         img="../images/electrical.jpg"
+         title="ELECTRICAL MAINTENANCE"
+         description="Time:- 9 am to 10 am 
+         Date:- 16-05-2022" />
+
+         <Announcement
+         img="../images/plumbing.jpg"
+         title="WATER MAINTENANCE"
+         description="Time:- 11 am to 1 pm 
+         Date:- 18-04-2022" />
+
+        <Announcement
+         img="../images/PROPERTY.jpg"
+         title="PROPERTY MAINTENANCE"
+         description="Time:- 6 am to 7 pm 
+         Date:- 30-03-2022" />
+         </div>
+
     </div>
   );
 }
--- frontend/src/components/Announcement.js
+++ frontend/src/components/Announcement.js
@@ -1,25 +1,47 @@
 import React from "react";
-import img1 from "../images/electrical.jpg";
+
 import { Card, Button } from "react-bootstrap";
-function Announcement() {
+function App1(){
   return (
-    <div>
-      Announcement
-      <Card style={{ width: "18rem", padding: "1rem" }}>
-        <Card.Img variant="top" src={img1}></Card.Img>
-        <Card.Body
-          style={{ textAlign: "center", backgroundColor: "lightpink" }}
-        >
-          <Card.Title style={{ textDecoration: "underline" }}>
-            ELECTRICAL MAINTENANCE
-          </Card.Title>
-          <Card.Text>TIME : 9 am to 10 am</Card.Text>
-          <Card.Text>DATE : 16-05-2022</Card.Text>
-          <Button variant="primary">More Imformation</Button>
-        </Card.Body>
-      </Card>
+       <div className="wrapper">
+         <Announcement 
+         img="https://images-ext-1.discordapp.net/external/SXwUsPiqo-kNLH21vC6a1cydpVi090d_Z4E_-lrH0Fw/https/hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+         title="ELECTRICAL MAINTENANCE"
+         description="Time:- 9 am to 10 am 
+         Date:- 16-05-2022" />
+
+         <Announcement
+         //src="../images/plumbing.jpg"
+         title="WATER MAINTENANCE"
+         description="Time:- 11 am to 1 pm 
+         Date:- 18-04-2022" />
+
+        <Announcement
+         //src="../images/PROPERTY.jpg"
+         title="PROPERTY MAINTENANCE"
+         description="Time:- 6 am to 7 pm 
+         Date:- 30-03-2022" />
+         </div>
+
+
+        );
+}
+function Announcement(props) {
+  return (
+    <div className="Announcement">
+    <div className="Announcement_body">
+      <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" />
+      <h2 className="Announcement_title">{props.title}</h2>
+      <p className="Announcement_description">{props.description}</p>
+    </div>
+      <button className="Announcement_btn">More Imformation</button>
     </div>
   );
 }
 
 export default Announcement;
+
+
+
+
+
