167e4ff4e7c227a39d0b64a92c9a715ea43ffb0b
vashz <zatakiavashisth151@gmail.com>
2022-05-17 21:28:45 +0530

    Landning Page Updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,32 +5,39 @@ import { LinkContainer } from "react-router-bootstrap";
 import LoginForm from "./LoginForm";
 function NavBar() {
   return (
-    <Navbar bg="dark" style={{ position: "sticky" }} variant="dark" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home">
-          <a href="/"> ABC</a>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/Announcement">
-              <Nav.Link>Announcement</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/ReachUs">
-              <Nav.Link>ReachUs</Nav.Link>
-            </LinkContainer>
-          </Nav>
-          <Nav className="ms-auto">
-            <Nav.Link>
-              <LoginForm />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar
+        bg="dark"
+        style={{ position: "sticky" }}
+        variant="dark"
+        fixed="top"
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <a href="/"> ABC</a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/Announcement">
+                <Nav.Link>Announcement</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/ReachUs">
+                <Nav.Link>ReachUs</Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <LoginForm />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
