6453eefb7130429957a519022a6dc5041b41822c
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-05-17 20:31:30 +0530

    Carousel issue fixed

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -6,6 +6,7 @@ import img2 from "../images/buildingfn.jpg";
 import img3 from "../images/kidsplayarea.jpg";
 import img4 from "../images/societyoffice.jpg";
 import img5 from "../images/terrace.jpg";
+import "../css/Tp.css";
 
 export default function CarouselImages() {
   return (
@@ -16,44 +17,51 @@ export default function CarouselImages() {
           verticalAlign: "center",
         }}
       >
-        <Carousel.Item interval={1000} className="imgcarousel">
+        <Carousel.Item interval={1000} >
           <img
-            className="d-block w-100"
+            className="d-block w-100 adjust"
             src={img1}
             alt="Front of the building in day"
           />
-          <Carousel.Caption>
+          <Carousel.Caption className="cap">
             <h3>ABC Tower</h3>
             <p>Daylight</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000} >
+        <Carousel.Item interval={1000}>
           <img
-            className="d-block w-100"
+            className="d-block w-100 adjust"
             src={img2}
             alt="Front Of the building in night"
           />
-          <Carousel.Caption>
+          <Carousel.Caption className="cap">
             <h3>ABC TOWER</h3>
             <p>Moon Light</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1500} className="imgcarousel">
-          <img className="d-block w-100" src={img5} alt="Society Office" />
-          <Carousel.Caption>
+        <Carousel.Item interval={1500}>
+          <img className="d-block w-100 adjust" src={img5} alt="Society Office" />
+          <Carousel.Caption className="cap">
             <h3>Terrace</h3>
+            <p>Plush Decor</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1500} >
-          <img className="d-block w-100 imagecarousel" src={img4} alt="Society Office" />
-          <Carousel.Caption>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100 adjust"
+            src={img4}
+            alt="Society Office"
+          />
+          <Carousel.Caption className="cap">
             <h4>Society Office</h4>
+            <p>Timings: 10:00-18:00</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500} className="imgcarousel">
-          <img className="d-block w-100" src={img3} alt="Kids Play Area" />
-          <Carousel.Caption>
+        <Carousel.Item interval={500}>
+          <img className="d-block w-100 adjust" src={img3} alt="Kids Play Area" />
+          <Carousel.Caption className="cap">
             <h3>Kids Play Area</h3>
+            <p>Age: Under 12</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
