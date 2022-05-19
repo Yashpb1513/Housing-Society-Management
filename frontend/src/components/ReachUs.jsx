d41647867ecc27b228301572fcbc6184e906af58
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-05-19 18:35:25 +0530

    ReachUs modified

--- frontend/src/components/ReachUs.jsx
+++ frontend/src/components/ReachUs.jsx
@@ -1,7 +1,7 @@
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import React, { Component } from "react";
 import logo from "../images/icons8-marker-storm-32.png";
-import img1 from "../images/societyoffice.jpg";
+import img1 from "../images/house.gif";
 import { GiRotaryPhone } from "react-icons/gi";
 import { IoIosMail } from "react-icons/io";
 import "../css/Carousel.css";
@@ -85,18 +85,21 @@ export class ReachUs extends Component {
             </a>
           </div>
         </InfoWindow>
-        <div
-          style={{
-            width: "100%",
-            height: "50%",
-            margin: "0 auto",
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={7}
+          sx={{
             backgroundColor: "#444445",
-            paddingLeft: "10%",
-            color: "white",
-            paddingTop: "2%",
+            padding: "1rem",
+            backgroundImage:
+              "linear-gradient(to bottom, black, white)",
           }}
         >
-          <div style={{ display: "inline-block", float: "left", width: "33%" }}>
+          <Grid item className="whiteAdjust">
             <h2>Society Office: </h2>
             <p>
               B-Wing 1<sup>st</sup> Floor, Lucky Tower.
@@ -111,12 +114,84 @@ export class ReachUs extends Component {
                 abcsociety@gmail.com
               </a>
             </p>
-          </div>
-          <div style={{ display: "inline-block", float: "left", width: "" }}>
+          </Grid>
+          <Grid item sx={{padding: "5px"}}>
+            <img
+              src={img1}
+              alt="Society Office"
+              style={{ width: "25rem", borderRadius: "15%", }}
+            />
+          </Grid>
+          <Grid item>
             <Grid
               style={{
                 color: "black",
-                width:"400px",
+                width: "400px",
+                backgroundColor: "transparent",
+                margin: "0 4px",
+                padding: "12px",
+                borderRadius: "20px",
+              }}
+              container
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={3}
+            >
+              <Grid item>
+                <label for="message" style={{fontSize: "2rem"}}>Enter your Queries</label>
+              </Grid>
+              <Grid item style={{ height: "auto", padding: "0px" }}>
+                <div class="md-form" style={{ backgroundColor: "transparent", textAlign: "center" }}>
+                  <i class="fas fa-pencil-alt prefix"></i>
+                  <textarea
+                    id="form10"
+                    class="md-textarea form-control"
+                    rows="2"
+                  ></textarea>
+                  {/* <br /> */}
+                  <Button type="submit" sx={{color: "black"}}>Submit</Button>
+                </div>
+              </Grid>
+              {/* <Grid item>
+                <Button type="submit">Submit</Button>
+              </Grid> */}
+            </Grid>
+          </Grid>
+          
+        </Grid>
+        {/* <div
+          style={{
+            width: "100%",
+            height: "50%",
+            margin: "0 auto",
+            backgroundColor: "#444445",
+            paddingLeft: "10%",
+            color: "white",
+            paddingTop: "2%",
+          }}
+        > */}
+        {/* <div style={{ display: "inline-block", float: "left", width: "33%" }}>
+            <h2>Society Office: </h2>
+            <p>
+              B-Wing 1<sup>st</sup> Floor, Lucky Tower.
+            </p>
+            <p className="anchoradjust">
+              <GiRotaryPhone style={{ fontSize: "30px", marginRight: "5px" }} />
+              <a href="tel:02212345678">(022)1234-5678</a>
+            </p>
+            <p className="anchoradjust">
+              <IoIosMail style={{ fontSize: "30px", marginRight: "5px" }} />
+              <a href="mailto:abcsociety@gmail.com?subject=Enquiry">
+                abcsociety@gmail.com
+              </a>
+            </p>
+          </div> */}
+        {/* <div style={{ display: "inline-block", float: "left", width: "" }}> */}
+        {/* <Grid
+              style={{
+                color: "black",
+                width: "400px",
                 backgroundColor: "#ffffff9c",
                 margin: "0 4px",
                 padding: "12px",
@@ -131,24 +206,30 @@ export class ReachUs extends Component {
               <Grid item>
                 <label for="message">Enter your Quries:</label>
               </Grid>
-              <Grid item style={{height:"200px"}}>
-                <textarea placeholder="Type here..." id="message" style={{height: "200px", width: "300px", alignItems:"center"}}>
-
-                </textarea>
+              <Grid item style={{ height: "200px" }}>
+                <textarea
+                  placeholder="Type here..."
+                  id="message"
+                  style={{
+                    height: "200px",
+                    width: "300px",
+                    alignItems: "center",
+                  }}
+                ></textarea>
               </Grid>
               <Grid item>
                 <Button type="submit">Submit</Button>
               </Grid>
-            </Grid>
-          </div>
-          <div style={{ display: "inline-block", float: "left", width: "30%" }}>
+            </Grid> */}
+        {/* </div> */}
+        {/* <div style={{ display: "inline-block", float: "left", width: "30%" }}>
             <img
               src={img1}
               alt="Society Office"
               style={{ width: "100%", margin: "0 20%", borderRadius: "15%" }}
             />
           </div>
-        </div>
+        </div> */}
       </Map>
     );
   }
