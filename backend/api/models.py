a4d9565681f9660efc9c52a5ff604d894dfba694
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-05-30 00:28:53 +0530

    Backend updated

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from matplotlib import image
 
 # Create your models here.
 class Usr(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
+
+class Announcements(models.Model):
+    header = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+    date = models.DateField()
+    image = models.CharField(max_length=100)
+    disabled = models.BooleanField(default=False)
+    alt = models.CharField(max_length=100)
+
+class Queries(models.Model):
+    email = models.CharField(max_length=100)
+    query = models.CharField(max_length=100)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import createUser
+from .views import createUser, Announcement, query, getUser
 
 urlpatterns = [
     path('api/', createUser),
+    path('announce/', Announcement),
+    path('query/', query),
+    path('getuser/', getUser)
 ]
